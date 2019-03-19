3939bb4b1aa81d941923328c1602b6dedfa46964
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2019-03-19 16:15:03 +0700

    FIrst COmmit

---
+++ app/src/main/java/com/refresh/cashierpropos/techicalservices/NoDaoSetException.java
@@ -0,0 +1,13 @@
+package com.refresh.cashierpropos.techicalservices;
+
+/**
+ * Exception of when trying to getInstance() without inject its DAO.
+ * 
+ * @author Refresh Team
+ *
+ */
+public class NoDaoSetException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+}
